caaef461ef8b32a27f6e2edcb74c45dcde2f1eeb
unsel <tunsel1@binghamton.edu>
2020-07-28 10:54:30 +0300

    building the skeleton

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import {connect} from 'react-redux';
+
+
+const Layout = props => {
+
+    return (
+        <div>
+            <p>Layout</p>
+        </div>
+    )
+}
+
+export default Layout;
